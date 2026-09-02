54b3d800eef8f159db52e06ecdcdcec979ee1703
Giovanni M. Leo <giovannileo100@gmail.com>
2026-09-02 16:58:41 +0800

    fix: restrict CORS allowed origins to known localhost hosts
    Allow only http://127.0.0.1:5500 and http://localhost:5500 as CORS
    origins by reflecting the request's Origin header only when it matches
    one of the whitelisted hosts.

--- src/main/java/com/svi/tictactoewebservice/config/CorsFilter.java
+++ src/main/java/com/svi/tictactoewebservice/config/CorsFilter.java
@@ -15,7 +15,11 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
 
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+        String origin = requestContext.getHeaderString("Origin");
+
+        if ("http://127.0.0.1:5500".equals(origin) || "http://localhost:5500".equals(origin)) {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
+        }
 
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
 
