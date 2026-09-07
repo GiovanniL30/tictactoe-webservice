51ef9fe03bd2d76316099ee868bb5fab16913707
Giovanni M. Leo <giovannileo100@gmail.com>
2026-09-07 18:04:57 +0800

    chore: updated base path endpoint to /api

--- src/main/java/com/svi/tictactoewebservice/App.java
+++ src/main/java/com/svi/tictactoewebservice/App.java
@@ -3,7 +3,7 @@ package com.svi.tictactoewebservice;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/rest")
+@ApplicationPath("/api")
 public class App extends Application {
 
 }
