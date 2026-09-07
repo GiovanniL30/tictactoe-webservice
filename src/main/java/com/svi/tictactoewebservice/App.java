0b7bec53c4dda408711eb1de197f2f6f322d412b
Giovanni M. Leo <giovannileo100@gmail.com>
2026-09-07 09:14:46 +0800

    chore: changed app route from /api to /rest

--- src/main/java/com/svi/tictactoewebservice/App.java
+++ src/main/java/com/svi/tictactoewebservice/App.java
@@ -3,7 +3,7 @@ package com.svi.tictactoewebservice;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/rest")
 public class App extends Application {
 
 }
